1edeb5722b3ad3ec1c76f6110342a2016c6a0121
billven8197 <billven@student.chalmers.se>
2023-09-26 14:46:38 +0200

    Refractoring of transmitter.m.Formatting and change of relevant variables.

--- Chat_PC_A/transmitter.m
+++ Chat_PC_A/transmitter.m
@@ -6,42 +6,46 @@
 %message='hello';
 %bin= dec2bin(message);
 %pack=bin;
-fc=6000;
-pack = randsrc(1,N,[0 1]);
-s=transmitter1(pack, fc);
+fc = 6000;
+pack = randsrc(1, N, [0 1]);
+s = transmitter1(pack, fc);
 
-function s=transmitter1(pack,fc)
+function pulse_train = transmitter1(pack, fc)
 
-fs = 5000; %sampling frequency
-Tsamp = 1/fs;
-Rb = 500; % bit rate [bit/sec]
-N = 432; % number of bits to transmit
-% Constellation or bit to symbol mapping
-const = [(1 + 1i) (1 - 1i) (-1 -1i) (-1 + 1i)]/sqrt(2); % Constellation 1 - QPSK/4-QAM
-symbs = N/log2(length(const));
+    fs = 20000; %sampling frequency
+    R_symb = 400; % Symbol rate
+    Q = floor(fs / R_symb); %Q=fsfd, samples per symbol
+    fs = Q * R_symb;
 
-M = length(const);                                      % Number of symbols in the constellation
-bpsymb = log2(M);                                        % Number of bits per symbol
-fsymb = Rb/bpsymb;                                          % Symbol rate [symb/s]
-fsfd = round(fs/fsymb);                                       % Number of samples per symbol (choose fs such that fsfd is an integer for simplicity) [samples/symb]
+    roll_off = 0.35;
+    span = 6;
 
-a = randsrc(1,N,[0 1]);
-preamble=[1,1,1,1,1,0,0,1,1,0,1,0,1];
+    Tsamp = 1 / fs;
+    N = 432; % number of bits to transmit
+    % Constellation or bit to symbol mapping
+    const = [(1 + 1i) (1 - 1i) (-1 -1i) (-1 + 1i)] / sqrt(2); % Constellation 1 - QPSK/4-QAM
 
-%a =pack ; % Information bits
-%assume pack is ASCII code
+    M = length(const); % Number of symbols in the constellation
+    bpsymb = log2(M); % Number of bits per symbol % Symbol rate [symb/s]
+    % Number of samples per symbol (choose fs such that fsfd is an integer for simplicity) [samples/symb]
 
-a=[preamble,a];
-m = buffer(a, bpsymb)';                           % Group bits into bits per symbol
-m = bi2de(m, 'left-msb')'+1;           % Bits to symbol index 01=(2^0+2^1)=3(+1 constallation)
-x = const(m);                                     % Look up symbols using the indices
+    a = randsrc(1, N, [0 1]);
+    preamble = [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1];
 
-x_upsample = upsample(x, fsfd);
-% Space the symbols fsfd apart, to enable pulse shaping using conv.
-span = 6;
-[pulse, t] = rcpuls(0.35,1/fsymb,fs,span);
-s = conv(pulse,x_upsample);
-%or maybe fft
+    %a =pack ; % Information bits
+    %assume pack is ASCII code
+
+    a = [preamble, a];
+    m = buffer(a, bpsymb)'; % Group bits into bits per symbol
+    m = bi2de(m, 'left-msb')' + 1; % Bits to symbol index 01=(2^0+2^1)=3(+1 constallation)
+    x = const(m); % Look up symbols using the indices
+
+    x_upsample = upsample(x, Q);
+    % Space the symbols fsfd apart, to enable pulse shaping using conv.
+
+    [pulse, t] = rcpuls(roll_off, 1 / R_symb, fs, span);
+    pulse_train = conv(pulse, x_upsample);
+    %or maybe fft
 %{
 npad = length(pulse)+length(x_upsample)-1;
 % Calculate the FFT of both signals
@@ -58,15 +62,15 @@ subplot(2,2,2); plot(t_vec,imag(s), 'b');
 title('imag')
 %}
 
-tx_signal=s.*exp(-1i*pi*fc*(0:1:length(s)-1)*Tsamp);  %carrier 
-%tx_signal_real=real(s).*sqrt(2)*cos(2*pi*fc*(0:1:length(s)-1)*Tsamp);
-%tx_signal_imag=imag(s).*sqrt(2)*sin(2*pi*fc*(0:1:length(s)-1)*Tsamp);
-%tx_signal=tx_signal_real+tx_signal;
-%tx_signal=real(tx_signal);        %take the real part
-tx_signal=tx_signal./max(abs(tx_signal));   %normalize it
+    tx_signal = pulse_train .* exp(-1i * pi * fc * (0:1:length(pulse_train) - 1) * Tsamp); %carrier
+    %tx_signal_real=real(s).*sqrt(2)*cos(2*pi*fc*(0:1:length(s)-1)*Tsamp);
+    %tx_signal_imag=imag(s).*sqrt(2)*sin(2*pi*fc*(0:1:length(s)-1)*Tsamp);
+    %tx_signal=tx_signal_real+tx_signal;
+    %tx_signal=real(tx_signal);        %take the real part
+    tx_signal = tx_signal ./ max(abs(tx_signal)); %normalize it
 
-player =audioplayer(tx_signal, fs);       
-playblocking(player)
+    player = audioplayer(tx_signal, fs);
+    playblocking(player)
 
 %{
 t_vec = Tsamp*(0:1:length(s)-1);
@@ -75,6 +79,5 @@ title('tx_real')
 subplot(2,2,4); plot(t_vec+fc,imag(tx_signal), 'b');
 title('tx_imag')
 %}
-
 
 end
