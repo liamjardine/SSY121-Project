ce406a41e0b373743f438dfb13d3595b71515221
billven8197 <billven@student.chalmers.se>
2023-10-09 15:34:23 +0200

    Cleanup in comments

--- Chat_PC_A/receiver.m
+++ Chat_PC_A/receiver.m
@@ -1,32 +1,32 @@
 % RECEIVER
-function [audio_recorder] = receiver(fc)
-    fs = 20000; %Goal sampling frequency
-    R_symb = 150; %TODO: Choose better wrt frequency mask
-    Q = floor(fs / R_symb); % Samples per symbol
-    fs = R_symb * Q; % Decided sampling frequency, everything is int
-    callback_interval = 0.25; % how often the function should be called in seconds
-    min_eye_res = 40; % Minimum Q to display in eye diagram. Makes GUI faster
+function [audio_recorder] = receiver(f_carrier)
+    fs = 20000;     % Goal sampling frequency
+    R_symb = 150;   % Symbol rate
+    Q = floor(fs / R_symb);     % Samples per symbol
+    fs = R_symb * Q;    % Decided sampling frequency, everything is int
+    callback_interval = 0.25;   % how often the function should be called in seconds
+    min_eye_res = 40;   % Minimum Q to display in eye diagram. Makes GUI somewhat faster
 
 
-    assert(fs / 2 > fc, "Too low sampling frequency to abide Nyquist.")
+    assert(fs / 2 > f_carrier, "Too low sampling frequency to abide Nyquist.")
 
-    audio_recorder = audiorecorder(fs, 24, 1); % create the recorder
+    audio_recorder = audiorecorder(fs, 24, 1);  % Create the recorder
 
-    %attach callback function
-    set(audio_recorder, 'TimerPeriod', callback_interval, 'TimerFcn', @audioTimerFcn); % attach a function that should be called every second, the function that is called is specified below.
+    % Attach callback function
+    set(audio_recorder, 'TimerPeriod', callback_interval, 'TimerFcn', @audioTimerFcn);
 
-    %ADD USER DATA FOR CALLBACK FUNCTION (DO NOT CHANGE THE NAMES OF THESE VARIABLES!)
-    audio_recorder.UserData.receive_complete = 0; % this is a flag that the while loop in the GUI will check
-    audio_recorder.UserData.pack = []; %allocate for data package
-    audio_recorder.UserData.pwr_spect = []; %allocate for PSD
-    audio_recorder.UserData.const = []; %allocate for constellation
-    audio_recorder.UserData.eyed = []; %allocate for eye diagram
+    % ADD USER DATA FOR CALLBACK FUNCTION (DO NOT CHANGE THE NAMES OF THESE VARIABLES!)
+    audio_recorder.UserData.receive_complete = 0;   % This is a flag that the while loop in the GUI will check
+    audio_recorder.UserData.pack = [];      % Allocate for data package
+    audio_recorder.UserData.pwr_spect = [];     % Allocate for PSD
+    audio_recorder.UserData.const = [];     % Allocate for constellation
+    audio_recorder.UserData.eyed = [];  % Allocate for eye diagram
 
-    audio_recorder.UserData.fc = fc;
+    audio_recorder.UserData.f_carrier = f_carrier;
     audio_recorder.UserData.Q = Q;
     audio_recorder.UserData.R_symb = R_symb;
 
-    optimum_reduction = ceil(Q/min_eye_res);    %What to divide Q with
+    optimum_reduction = ceil(Q/min_eye_res);    % What to divide Q with
     divs = [];
     for i=1:optimum_reduction
         divs = [divs gcd(Q,i)]; % Get divisors without requiring Symbolic Toolbox
@@ -36,7 +36,7 @@ function [audio_recorder] = receiver(fc)
     % Divide Q with the largest divisor less or equal to or desired one.
     audio_recorder.UserData.best_Q_divisor = max(divs(divs<=optimum_reduction));
 
-    record(audio_recorder); %start recording
+    record(audio_recorder); % Start recording
 end
 
 % CALLBACK FUNCTION
@@ -48,23 +48,22 @@ function audioTimerFcn(recObj, event, handles)
     Tsample = 1 / f_sample;
 
     %%%%% Variables %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %TODO: Ensure consistency of roll off with TX
     preamble = zadoffChuSeq(859, 13)';
-    roll_off = 0.35;
-    span = 6;
-    PA_thresh = 1; % Placeholder
-    msg_to_keep = 2;
-    %%%% Variables %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    roll_off = 0.35;    % Roll off factor for root raised cosine
+    span = 6;           % Number of T_s to keep of symbol
+    PA_thresh = 1;      % Threshold for when to preamble is above noise
+    msg_to_keep = 2;    % Number of messages to keep in buffer
+    %%%%% Variables %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    R_symb = recObj.UserData.R_symb; % Symbol rate [symb/s]
-    T_symb = 1 / R_symb; % Symbol time [s/symb]
-    Q = recObj.UserData.Q; % Number of samples per symbol (choose fs such that Q is an integer) [samples/symb]
-    f_carrier = recObj.UserData.fc;
-    N_bits = 432; % number of bits
-    const = [(1 + 1i) (1 - 1i) (-1 + 1i) (-1 -1i)] / sqrt(2); % Constellation QPSK/4-QAM, [00 01 10 11], GRAY-encoded
-    quad_to_bits = [0 0; 1 0; 1 1; 0 1]; % Quadrant number converted to bits
+    R_symb = recObj.UserData.R_symb;    % Symbol rate [symb/s]
+    T_symb = 1 / R_symb;    % Symbol time [s/symb]
+    Q = recObj.UserData.Q;  % Number of samples per symbol (choose fs such that Q is an integer) [samples/symb]
+    f_carrier = recObj.UserData.f_carrier;
+    N_bits = 432;   % number of bits
+    const = [(1 + 1i) (1 - 1i) (-1 + 1i) (-1 -1i)] / sqrt(2);   % Constellation QPSK/4-QAM, [00 01 10 11], GRAY-encoded
+    quad_to_bits = [0 0; 1 0; 1 1; 0 1];    % Quadrant number converted to bits
 
-    M = length(const); % Number of symbols in the constellation
+    M = length(const);  % Number of symbols in the constellation
     bits_per_symbol = log2(M);
     N_symbols = N_bits / bits_per_symbol;
 
@@ -73,7 +72,7 @@ function audioTimerFcn(recObj, event, handles)
     samples_to_keep = ceil(samples_per_msg * msg_to_keep);
 
 
-    rec_data = getaudiodata(recObj)'; % As row vector
+    rec_data = getaudiodata(recObj)';
     try
         rec_data = rec_data(1, (end - samples_to_keep):end);
     catch
@@ -81,7 +80,7 @@ function audioTimerFcn(recObj, event, handles)
     end
 
     rec_data_downConv = rec_data .* exp(1i * 2 * pi * f_carrier .* (0:length(rec_data) - 1) * Tsample);
-    rec_data_lowpass = lowpass(rec_data_downConv, f_carrier, f_sample); % Trim LPF if we have noise problems
+    rec_data_lowpass = lowpass(rec_data_downConv, f_carrier, f_sample);     % Trim LPF if we have noise problems
 
     preamble_upsample = upsample(preamble, Q);
     [pulse, ~] = rtrcpuls(roll_off, T_symb, f_sample, span);
@@ -121,19 +120,16 @@ function audioTimerFcn(recObj, event, handles)
     bits = quad_to_bits(quadrant_number, :);
     bits = reshape(bits', 1, []);
 
-    %------------------------------------------------------------------------------
-    % HOW TO SAVE DATA FOR THE GUI
-    %   NOTE THAT THE EXAMPLE HERE IS ONLY USED TO SHOW HOW TO OUTPUT DATA
-    %------------------------------------------------------------------------------
 
-    % Step 1: save the estimated bits
-    recObj.UserData.pack = bits;
 
-    % Step 2: save the sampled symbols
-    recObj.UserData.const = MF_sampled_rotated / max(abs(MF_sampled_rotated));
+    %%%%% GUI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % The estimated bits
+    recObj.UserData.pack = bits;    
 
-    % Step 3: provide the matched filter output for the eye diagram (Note here no match filter is used. You should do that)
+    % The sampled symbols (constellation)
+    recObj.UserData.const = MF_sampled_rotated / max(abs(MF_sampled_rotated));  
     
+    % Eye diagram
     best_Q_divisor = recObj.UserData.best_Q_divisor;    % Downsample Q for faster view of eyediagram
     MF_to_eye = MF_output(data_indices(1):best_Q_divisor:data_indices(end)) .* exp(-1i * (phase_shift / 180) * pi);
 
@@ -141,12 +137,12 @@ function audioTimerFcn(recObj, event, handles)
     recObj.UserData.eyed.fsfd = Q/best_Q_divisor;
 
 
-    % Step 4: Compute the PSD and save it.
+    % PSD 
     % !!!! NOTE !!!! the PSD should be computed on the BASE BAND signal BEFORE matched filtering
-    [pxx, f] = pwelch(rec_data_lowpass, 1024, 768, 1024, f_sample); % note that pwr_spect.f will be normalized frequencies
-    f = fftshift(f); %shift to be centered around fs
-    f(1:length(f) / 2) = f(1:length(f) / 2) - f_sample; % center to be around zero
-    p = fftshift(10 * log10(pxx / max(pxx))); % shift, normalize and convert PSD to dB
+    [pxx, f] = pwelch(rec_data_lowpass, 1024, 768, 1024, f_sample);     % Note that pwr_spect.f will be normalized frequencies
+    f = fftshift(f);    % Shift to be centered around fs
+    f(1:length(f) / 2) = f(1:length(f) / 2) - f_sample;     % Center to be around zero
+    p = fftshift(10 * log10(pxx / max(pxx)));   % Shift, normalize and convert PSD to dB
     recObj.UserData.pwr_spect.f = f;
     recObj.UserData.pwr_spect.p = p;
 
